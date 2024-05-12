80ecbdc941e6c091d65b432a69de7a65007f4911
shadowgarden1314 <131418013+shadowgarden1314@users.noreply.github.com>
2024-05-12 16:12:34 +0800

    Update NavButtonGroup.js

--- themes/hexo/components/NavButtonGroup.js
+++ themes/hexo/components/NavButtonGroup.js
@@ -20,7 +20,7 @@ const NavButtonGroup = (props) => {
             title={`${category.name}`}
             href={`/category/${category.name}`}
             passHref
-            className='text-center shadow-text w-full sm:w-4/5 md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 hover:scale-105 transform'>
+            className='text-center shadow-text w-full sm:w-4/5 md:mx-6 md:w-0 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 hover:scale-105 transform'>
                {category.name}
             </Link>
         )
